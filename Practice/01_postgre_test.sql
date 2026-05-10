742ae7477dc736a26ef12ee6c490e275832dca7d
Yi Sun <yi.sun@rwth-aachen.de>
2026-05-10 19:26:07 +0200

    Add comment

--- Practice/01_postgre_test.sql
+++ Practice/01_postgre_test.sql
@@ -1,15 +1,20 @@
-SELECT current_database(), current_user, version();
+SELECT current_database(), current_user, version(); 
+--作用是测试：当前连接的是哪个数据库;当前用户是谁; PostgreSQL 版本是什么
 
+--Creat table
 CREATE TABLE test_students (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50),
     age INT
 );
 
+-- Insert values
 INSERT INTO test_students (name, age)
 VALUES
 ('Alice', 24),
 ('Bob', 26),
 ('Charlie', 23);
 
+-- Search for this table
 SELECT * FROM test_students;
+
